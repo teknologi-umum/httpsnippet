9b1fdf2b59d80b445243e2c707a031934924f6bb
elianiva <dicha.arkana03@gmail.com>
2022-03-13 08:34:21 +0700

    refactor: use overloading on public API

--- src/helpers/code-builder.ts
+++ src/helpers/code-builder.ts
@@ -12,14 +12,18 @@ import util from 'util'
 export default class CodeBuilder {
   private _code: (string | null)[];
 
-  constructor(private _indentation = "", private _lineJoin = "\n") {
+  constructor(private _indentation: string, private _lineJoin = "\n") {
+    if (typeof _indentation !== "string") {
+      this._indentation = ""
+    }
+
     this._code = []
   }
 
   /**
    * Add given indentation level to given string and format the string (variadic)
    * @param {number} [indentationLevel=0] - Desired level of indentation for this line
-   * @param {string} line - Line of code. Can contain formatting placeholders
+   * @param {string} lineOrArgs - Line of code. Can contain formatting placeholders
    * @param {...anyobject} - Parameter to bind to `line`'s formatting placeholders
    * @return {string}
    *
@@ -35,48 +39,87 @@ export default class CodeBuilder {
    *   builder.buildLine(2, 'console.log("%s %s")', 'hello', 'world')
    *   // returns: 'console.log("\t\thello world")'
    */
-  buildLine(line: null): null;
-  buildLine(line: string, ...args: any[]): string;
-  buildLine(indentationLevel: number, line: string, ...args: any[]): string;
-  buildLine(indentLevelOrLine: string | number | null, line?: any, ...args: any[]): string | null {
-    if (indentLevelOrLine === null) return null
-
-    if (typeof indentLevelOrLine === "string") {
-      let data = args;
-      if (typeof line !== "undefined") {
-        data.unshift(line)
-      }
-
-      return util.format(indentLevelOrLine, ...args)
-    }
-
-    let lineIndentation = this._indentation.repeat(indentLevelOrLine);
-
+  private buildLine(
+    indentLevel: number,
+    line: string | null,
+    ...args: any[]
+  ): string | null {
+    if (line === null) return null
+    let lineIndentation = this._indentation.repeat(indentLevel)
     return util.format(lineIndentation + line, ...args)
   }
 
   /**
    * Invoke buildLine() and add the line at the top of current lines
-   * @param {number} [indentationLevel=0] Desired level of indentation for this line
-   * @param {string} line Line of code
+   * @param line The content of the line. Null will be treated as an empty line
+   * @param args Placeholder value that will be passed to NodeJS `util.format`
    * @return {this}
    */
-  unshift(): this {
-    // @ts-ignore
-    this._code.unshift(this.buildLine(...arguments))
+  unshift(line: string | null, ...args: any[]): this;
+  /**
+   * Invoke buildLine() and add the line at the top of current lines
+   * @param indentationLevel Current line indentation level
+   * @param line The content of the line. Null will be treated as an empty line
+   * @param args Placeholder value that will be passed to NodeJS `util.format`
+   * @return {this}
+   */
+  unshift(indentationLevel: number, line: string, ...args: any[]): this;
+  unshift(indentLevelOrLine: any, lineOrArgs?: any, ...args: any[]): this {
+    if (indentLevelOrLine === null) {
+      this._code.unshift(this.buildLine(0, null))
+    }
+
+    if (typeof indentLevelOrLine === "number") {
+      this._code.unshift(this.buildLine(indentLevelOrLine, lineOrArgs, ...args));
+    }
+
+    if (typeof indentLevelOrLine === "string") {
+      let data = args || [];
+      if (typeof lineOrArgs !== "undefined") {
+        data.unshift(lineOrArgs)
+      }
+
+      this._code.unshift(this.buildLine(0, indentLevelOrLine, ...data))
+    }
 
     return this
   }
 
   /**
    * Invoke buildLine() and add the line at the bottom of current lines
-   * @param {number} [indentationLevel=0] Desired level of indentation for this line
-   * @param {string} line Line of code
+   * @param line The content of the line. Null will be treated as an empty line
+   * @param args Placeholder value that will be passed to NodeJS `util.format`
    * @return {this}
    */
-  push() {
+  push(line: string | null, ...args: any[]): this;
+  /**
+   * Invoke buildLine() and add the line at the bottom of current lines
+   * @param indentationLevel Current line indentation level
+   * @param line The content of the line. Null will be treated as an empty line
+   * @param args Placeholder value that will be passed to NodeJS `util.format`
+   * @return {this}
+   */
+  push(indentationLevel: number, line: string, ...args: any[]): this;
+  push(indentLevelOrLine: any, lineOrArgs?: any, ...args: any[]): this {
+    if (indentLevelOrLine === null) {
+      this._code.push(this.buildLine(0, null))
+    }
+
+    if (typeof indentLevelOrLine === "number") {
+      this._code.push(this.buildLine(indentLevelOrLine, lineOrArgs, ...args));
+    }
+
+    if (typeof indentLevelOrLine === "string") {
+      let data = args || [];
+      if (typeof lineOrArgs !== "undefined") {
+        data.unshift(lineOrArgs)
+      }
+
+      this._code.push(this.buildLine(0, indentLevelOrLine, ...data))
+    }
+
     // @ts-ignore
-    this._code.push(this.buildLine(...arguments))
+    // this._code.push(this.buildLine(...arguments))
 
     return this
   }
@@ -86,7 +129,6 @@ export default class CodeBuilder {
    */
   blank(): this {
     this._code.push(null)
-
     return this
   }
 
